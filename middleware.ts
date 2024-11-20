015759efc2b9069ac5fb6788112b0651c739dbf1
Wong Shao Xuan <shaoxuanhinhua@gmail.com>
2024-11-20 23:52:15 +0800

    skip middleware for Stripe webhook route

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/api/webhook/stripe") {
+    return NextResponse.next(); // Skip middleware for this route
+  }
   return await updateSession(request);
 }
 
